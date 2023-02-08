c634c3d8cdda64bf199eca8ec5d0c5f4c0a30aa4
Thibaut Dusautoir <tdusautoir@myges.fr>
2023-02-08 14:59:32 +0100

    add GlobalStyle.js

---
+++ app/src/App.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Side = styled.aside`
+  color: red;
+  position: fixed;
+  width: 240px;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  background-color: #1f2123;
+`;
+ 
+export const Main = styled.main`
+  color: green;
+  background-color: #232527;
+  height: 100vh;
+  margin-left: 240px;
+`;
